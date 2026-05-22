d7ab3ee4480d3353d0ae77160b76a7fc874478bf
simoneparente <parentesimone.01@gmail.com>
2026-05-22 19:15:09 +0200

    feat(workflow/tests): add H2 database dependency and configuration for workflow and future testing

--- bugboard-backend/src/test/java/it/unina/bugboard/bugboard_backend/BugBoard26ApplicationTests.java
+++ bugboard-backend/src/test/java/it/unina/bugboard/bugboard_backend/BugBoard26ApplicationTests.java
@@ -2,8 +2,10 @@ package it.unina.bugboard.bugboard_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BugBoard26ApplicationTests {
 
 	@Test
