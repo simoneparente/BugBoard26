23553616dc018c2e7e216ffb1398f9b7e8fadc34
simoneparente <parentesimone.01@gmail.com>
2026-05-24 14:49:12 +0200

    fix(dto): change email field from final to mutable in AuthRequest class

--- bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/dto/AuthRequest.java
+++ bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/dto/AuthRequest.java
@@ -4,12 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class AuthRequest {
     @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required")
-    private final String email;
+    private String email;
     @NotBlank(message = "Password is required")
     private String password;
 }
