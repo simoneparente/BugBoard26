7476aecacfa31b674878cfc0c0fa4692c43234f9
bickpenna <mario.penna00@gmail.com>
2026-06-12 08:22:42 +0200

    feat(exception): add FileStorageException and handle it in GlobalExceptionHandler

---
+++ bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/exception/FileStorageException.java
@@ -0,0 +1,12 @@
+package it.unina.bugboard.bugboard_backend.exception;
+
+public class FileStorageException extends RuntimeException {
+    
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
