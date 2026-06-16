d50b5ad350b4c8e26dd1c6a6c0583b65a31e7961
simoneparente <parentesimone.01@gmail.com>
2026-06-16 19:41:47 +0200

    feat(routes): implement auth guard and update routing configuration

--- bugboard-frontend/src/app/app.config.ts
+++ bugboard-frontend/src/app/app.config.ts
@@ -2,14 +2,29 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { AuthService } from './core/auth/auth-service';
+import { Observable } from 'rxjs';
+
+export function initializeApp(authService: AuthService): () => Observable<any> {
+  return () => authService.checkSession();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor])
+    ),
+    {
+      provide: 'APP_INITIALIZER',
+      useFactory: initializeApp,
+      deps: [AuthService],
+      multi: true
+    }
   ]
 };
--- bugboard-frontend/src/app/app.routes.ts
+++ bugboard-frontend/src/app/app.routes.ts
@@ -1,11 +1,32 @@
-import { Routes } from '@angular/router';
+import { Routes, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './core/auth/auth-service';
+import { LoginComponent } from './features/login.component/login.component';
 
-import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './features/dashboard.component/dashboard.component';
+
+const authGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+  if (authService.isAuthenticated()) {
+    return true;
+  }
+  
+  router.navigate(['/login']);
+  return false;
+};
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
         title: "BugBoard26 - Login"
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: "BugBoard26 - Dashboard",
+        canActivate: [authGuard]
     }
 ];
