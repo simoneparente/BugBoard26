bbb6571c336506b6c687553bd4934ee4c8139cd0
simoneparente <parentesimone.01@gmail.com>
2026-06-28 16:10:54 +0200

    feat(app): refactor app initialization to use provideAppInitializer and handle session check errors

--- bugboard-frontend/src/app/app.config.ts
+++ bugboard-frontend/src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthService } from './core/auth/auth-service';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 
 export function initializeApp(authService: AuthService): () => Observable<any> {
   return () => authService.checkSession();
@@ -17,11 +22,15 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-    {
-      provide: 'APP_INITIALIZER',
-      useFactory: initializeApp,
-      deps: [AuthService],
-      multi: true,
-    },
+    provideAppInitializer(async () => {
+      const authService = inject(AuthService);
+      try {
+        // firstValueFrom forces the Observable to resolve and allows us to catch errors during initialization.
+        await firstValueFrom(authService.checkSession());
+      } catch (error) {
+        // If the session check fails, we log a warning. This could be due to network issues or the user not being authenticated.
+        console.warn('Session check failed during initialization.');
+      }
+    }),
   ],
 };
