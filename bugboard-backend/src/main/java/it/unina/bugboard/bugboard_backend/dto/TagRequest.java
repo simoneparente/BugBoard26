1c3aed009a46f5651bd9d92c8607ac1533e3b6fb
Simone <107051698+simoneparente@users.noreply.github.com>
2026-07-24 16:48:19 +0200

    fix(dto): remove unused import in TagRequest

--- bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/dto/TagRequest.java
+++ bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/dto/TagRequest.java
@@ -1,13 +1,11 @@
 package it.unina.bugboard.bugboard_backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
 
 @Data
 @Builder
