40c2126a28a86eb0d840cdb95b1cce7af4d5535b
Simone <107051698+simoneparente@users.noreply.github.com>
2026-07-24 11:11:29 +0200

    refactor: update report endpoint documentation to use projectKey instead of projectId

--- bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/controller/ReportController.java
+++ bugboard-backend/src/main/java/it/unina/bugboard/bugboard_backend/controller/ReportController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/reports")
 @RequiredArgsConstructor
@@ -18,8 +16,10 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    // Endpoint to generate a report for a specific project
-    // Responds to: GET http://localhost:8080/api/reports/{projectId}
+    /**
+     * Endpoint to generate a report for a specific project
+     * Responds to: GET /api/reports/{projectKey}
+     */
     @GetMapping("/{projectKey}")
     public ResponseEntity<MonthlyProjectReportResponse> generateReport(@PathVariable String projectKey) {
         MonthlyProjectReportResponse response = reportService.generateReport(projectKey);
